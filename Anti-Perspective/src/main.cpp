cd60d9650aff797f7846ef6cb1aa9b8d256243d2
Kai Hoop <unrelatedstring@gmail.com>
2019-02-18 14:41:29 -0500

    Added code to select between different applications in main

--- Anti-Perspective/src/main.cpp
+++ Anti-Perspective/src/main.cpp
@@ -2,5 +2,17 @@
 #include "internal-header.hpp"
 
 int main(int argc, const char * argv[]) {
-	return run_viewer();
+	std::cout << "Select mode:\n";
+	std::cout << "s for the viewer with sliders,\n";
+	std::cout << "r to select ROIs for Facemark,\n";
+	std::cout << "d for real-time face detection.\n";
+	char mode;
+	std::cin >> mode;
+	switch (mode) {
+	case 's':
+		return run_viewer();
+	case 'r': //to add later
+	case 'd': //to add later
+	default: return 0;
+	}
 }
