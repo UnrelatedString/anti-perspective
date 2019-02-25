4a02a06266cd4027075678ee293c449d375a0316
Kai Hoop <unrelatedstring@gmail.com>
2019-02-25 16:53:07 -0500

    select.cpp is now indexed

---
+++ Anti-Perspective/src/select.cpp
@@ -0,0 +1,29 @@
+#include "internal-header.hpp"
+
+#include <iostream>
+#include <vector>
+#include <opencv2/face.hpp>
+#include <cmath>
+
+int select() {
+    cv::Ptr<cv::face::Facemark> facemark = cv::face::FacemarkLBF::create();
+    facemark->loadModel("/Users/immensityofkaiq/Downloads/lbfmodel.yaml");
+    cv::VideoCapture cam(0);
+    cv::Mat frame;
+    cam.read(frame);
+    cam.read(frame);
+    cv::Rect r;
+    std::vector<cv::Rect> i{ {0, 0, 0, 0} };
+    std::vector< std::vector<cv::Point2f> > o;
+    while ((r = cv::selectROI("Image", frame)).height) {
+        i[0] = r;
+        facemark->fit(frame, i, o);
+        cv::face::drawFacemarks(frame, o[0]);
+        cv::drawMarker(frame, o[0][27], {0,255,0});
+        cv::rectangle(frame, r, {0, 0, 255});
+        cv::imshow("Image", frame);
+        cv::waitKey(0);
+        cam.read(frame);
+    }
+    return 0;
+}
