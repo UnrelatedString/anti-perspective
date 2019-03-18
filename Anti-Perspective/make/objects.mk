044e4c738a85a46a74001b4b4f803776e780846a
Kai Hoop <unrelatedstring@gmail.com>
2019-03-18 14:40:05 -0400

    Switched off of automatically generated makefiles

---
+++ Anti-Perspective/make/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. For now.
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_objdetect.3.4.5 -lopencv_videoio.3.4.5 -lopencv_face.3.4.5 -lopencv_core.3.4.5 -lopencv_imgcodecs.3.4.5 -lopencv_imgproc.3.4.5 -lopencv_highgui.3.4.5
+
---
+++ Anti-Perspective/make/subdir.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Not that there's anything in it...
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+
+# Each subdirectory must supply rules for building sources it contributes
+
