73192ab972937d6a1bfe08e885ab74b7f0dbad9e
Kai Hoop <unrelatedstring@gmail.com>
2019-08-30 15:44:51 -0400

    Some stuff implemented for Transform

--- Anti-Perspective/src/transform.py
+++ Anti-Perspective/src/transform.py
@@ -1,8 +1,33 @@
 import numpy as np
+import cv2
+from math import sin, cos, radians
 from data_classes import *
 
 class Transform:
-    pass
+    def __init__():
+        self.d = 1
+        self.theta = 0
+    def get_mat():
+        a = radians(theta)
+        s = sin(a)
+        c = cos(a)
+        return np.array([
+            [c,   0, -s*d],
+            [0,   1, 0],
+            [s/d, 0, c]
+        ])
+    def __call__(size):
+        return np.array([
+                [1, 0, size[0]//2],
+                [0, 1, size[1]//2],
+                [0, 0, 1]
+            ]) @
+            get_mat() @
+            np.array([
+                [1, 0, -size[0]//2],
+                [0, 1, -size[1]//2],
+                [0, 0, 1]
+            ])
 
 class VirtualImage:
     pass
