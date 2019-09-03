b560ceba64ffeaa0075be625889fcfe15fd54aef
Kai Hoop <unrelatedstring@gmail.com>
2019-09-03 14:48:47 -0400

    Added self arguments

--- Anti-Perspective/src/frontend.py
+++ Anti-Perspective/src/frontend.py
@@ -1,7 +1,7 @@
 from transform import *
 
 class SliderFrontend:
-    def __init__(path):
+    def __init__(self, path):
         self.vi = VirtualImage(cv2.imread(path), (700,700))
         cv2.imshow('Source', vi.img)
         cv2.namedWindow('Result')
@@ -15,9 +15,9 @@ class SliderFrontend:
                            0,
                            180,
                            vi.update_angle)
-    def __call__():
+    def __call__(self):
         while cv2.waitKey(15) & 0xff != 27:
             cv2.imshow('Result', vi())
-    def __del__():
+    def __del__(self):
         cv2.destroyWindow('Source')
         cv2.destroyWindow('Result')
--- Anti-Perspective/src/transform.py
+++ Anti-Perspective/src/transform.py
@@ -4,10 +4,10 @@ from math import sin, cos, radians
 from data_classes import *
 
 class Transform:
-    def __init__():
+    def __init__(self):
         self.d = 1
         self.theta = 0
-    def get_mat():
+    def get_mat(self):
         a = radians(theta)
         s = sin(a)
         c = cos(a)
@@ -16,13 +16,13 @@ class Transform:
             [0,   1, 0],
             [s/d, 0, c]
         ])
-    def __call__(size):
+    def __call__(self, size):
         return np.array([
                 [1, 0, size[0]//2],
                 [0, 1, size[1]//2],
                 [0, 0, 1]
-            ]) @
-            get_mat() @
+            ]) @\
+            get_mat() @\
             np.array([
                 [1, 0, -size[0]//2],
                 [0, 1, -size[1]//2],
@@ -30,15 +30,15 @@ class Transform:
             ])
 
 class VirtualImage:
-    def __init__(img, size):
+    def __init__(self, img, size):
         self.img = cv2.resize(img, size, interpolation=cv2.INTER_NEAREST)
         self.size = size
         self.transform = Transform()
-    def update_distance(d):
+    def update_distance(self, d):
         transform.d = d
-    def update_angle(theta):
+    def update_angle(self, theta):
         transform.theta = theta
-    def __call__():
+    def __call__(self):
         return cv2.warpPerspective(img,
                                    transform(),
                                    size,
