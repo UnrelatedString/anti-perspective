b308eb6adae99e62522400322a65575604f0f269
Kai Hoop <unrelatedstring@gmail.com>
2019-09-03 14:43:35 -0400

    Might be runnable?

---
+++ Anti-Perspective/src/frontend.py
@@ -0,0 +1,23 @@
+from transform import *
+
+class SliderFrontend:
+    def __init__(path):
+        self.vi = VirtualImage(cv2.imread(path), (700,700))
+        cv2.imshow('Source', vi.img)
+        cv2.namedWindow('Result')
+        cv2.createTrackbar('distance',
+                           'Result',
+                           1,
+                           10000,
+                           vi.update_distance)
+        cv2.createTrackbar('angle',
+                           'Result',
+                           0,
+                           180,
+                           vi.update_angle)
+    def __call__():
+        while cv2.waitKey(15) & 0xff != 27:
+            cv2.imshow('Result', vi())
+    def __del__():
+        cv2.destroyWindow('Source')
+        cv2.destroyWindow('Result')
