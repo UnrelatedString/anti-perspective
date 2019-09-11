4a56c43addc655b5a3d58d753a25a3b9fe25ba7d
Kai Hoop <unrelatedstring@gmail.com>
2019-09-11 01:14:38 -0400

    A bit more tracker stuff added

--- Anti-Perspective/src/data_classes.py
+++ Anti-Perspective/src/data_classes.py
@@ -1,11 +1,11 @@
 class Observer:
-    def __init__(x, y, u):
+    def __init__(self, x, y, u):
         self.x = x
         self.y = y
         self.u = u
 
 class Location:
-    def __init__(d, theta, phi):
+    def __init__(self, d, theta, phi):
         self.d = d
         self.theta = theta
 ##        self.phi = phi #vertical
---
+++ Anti-Perspective/src/tracking.py
@@ -0,0 +1,9 @@
+import cv2
+
+class Tracker:
+    def __init__(self):
+        self.cam = cv2.VideoCapture(0)
+        cam.grab() # takes a frame or two for a good result
+        cam.grab()
+    def __call__(self):
+        pass
