364f94d44ae0d215bd2f7b1489b4e09d962f4990
Kai Hoop <unrelatedstring@gmail.com>
2019-09-11 16:10:19 -0400

    test.sh now tests tracker

--- Anti-Perspective/test.sh
+++ Anti-Perspective/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd src
-python3 -c "from frontend import SliderFrontend as f; f('../$1')()"
+python3 -c "from frontend import TrackerFrontend as f; from tracking import RepeatingDetector as d; f(d())()"
