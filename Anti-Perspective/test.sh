00c1b32ea4bb1e5412671c820ec4e1289cf2795b
Kai Hoop <unrelatedstring@gmail.com>
2019-09-13 15:15:41 -0400

    test.sh

--- Anti-Perspective/test.sh
+++ Anti-Perspective/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd src
-python3 -c "from frontend import TrackerFrontend as f; from tracking import FacemarkRepeatingDetector as d; f(d())()"
+python3 -c "from frontend import AntiPerspectiveFrontend as f; from tracking import FacemarkRepeatingDetector as d; f('../$1',d())()"
